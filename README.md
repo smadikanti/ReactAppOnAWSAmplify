499413f26b6a16cbf798187658462666753c9ab3
smadikanti <madikanti.sunil@gmail.com>
2020-10-27 01:09:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-z9gx4t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-z9gx4t)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello from a React App!</h1>
+      <p>This Application is deployed on AWS using AWS Amplify</p>
+    </div>
+  );
+}
